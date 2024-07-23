78d30785528105b6a4400c3c81345d5b85e30082
nicho <nap44794@uga.edu>
2024-07-23 13:55:11 -0400

    feat: implent sliders for control

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,39 +9,62 @@ import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 import java.lang.Math;
 
 public class App extends Application {
 
+    Slider yzSlider;
+    Slider xzSlider;
+    Slider xySlider;
+    Slider scaleSlider;
+    Slider xTransSlider;
+    Slider yTransSlider;
+    Button update;
+
+    Canvas canvas;
+    GraphicsContext gc;
+
     @Override
     public void start(Stage primaryStage) {
         // Create a canvas
-        Canvas canvas = new Canvas(600, 400);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
+        this.canvas = new Canvas(600, 400);
+        gc = canvas.getGraphicsContext2D();
 
 	VBox sliderBox = new VBox(8);
 
-	HBox yzBox = new HBox(8);
-	HBox xzBox = new HBox(8);
-	HBox xyBox = new HBox(8);
-	
-	Slider yzSlider = new Slider(0, Math.PI * 2, 0);
-	Slider xzSlider = new Slider(0, Math.PI * 2, 0);
-	Slider xySlider = new Slider(0, Math.PI * 2, 0);
+	HBox xTransBox = new HBox();
+	HBox yTransBox = new HBox();
+	HBox yzBox = new HBox();
+	HBox xzBox = new HBox();
+	HBox xyBox = new HBox();
+	HBox scaleBox = new HBox();
 
+	xTransSlider = new Slider(0, 500, 0);
+	yTransSlider = new Slider(0, 500, 0);
+	yzSlider = new Slider(0, Math.PI * 2, 0);
+	xzSlider = new Slider(0, Math.PI * 2, 0);
+	xySlider = new Slider(0, Math.PI * 2, 0);
+	scaleSlider = new Slider(0, 100, 1);
+	update = new Button("update");
+	update.setOnAction(e -> actionHandler());
+
+	xTransBox.getChildren().addAll(new Label("X Translation"), xTransSlider);
+	yTransBox.getChildren().addAll(new Label("Y Translation"), yTransSlider);
 	yzBox.getChildren().addAll(new Label("YZ Rotation"), yzSlider);
 	xzBox.getChildren().addAll(new Label("XZ Rotation"), xzSlider);
 	xyBox.getChildren().addAll(new Label("XY Rotation"), xySlider);
+	scaleBox.getChildren().addAll(new Label("Scale"), scaleSlider, update);
 	
-	sliderBox.getChildren().addAll(yzBox, xzBox, xyBox);
+	sliderBox.getChildren().addAll(xTransBox, yTransBox, yzBox, xzBox, xyBox, scaleBox);
 	
         // Set initial color and width for drawing
         gc.setStroke(Color.BLACK);
         gc.setLineWidth(2);
 
 	// draws an orthographic projection
-       	drawOrthographic(gc);
+       	drawOrthographic();
 
         // Create a border pane layout
         BorderPane root = new BorderPane();
@@ -57,32 +80,70 @@ public class App extends Application {
         primaryStage.show();
     }
 
-    private void drawLine(GraphicsContext gc, double startX, double startY, double endX, double endY) {
+    private void drawLine(double startX, double startY, double endX, double endY) {
         gc.strokeLine(startX, startY, endX, endY);
     }
 
-    private void drawOrthographic(GraphicsContext gc) {
-	OBJReader obj = new OBJReader("dodecahedron.obj");
-	for (int i = 0; i < obj.faces.length; i++) {
-	    int v1 = obj.faces[i].v1;
-	    int v2 = obj.faces[i].v2;
-	    int v3 = obj.faces[i].v3;
-	    int tX = 300;
-	    int tY = 200;
-	    int s = 50;
-
-	    drawLine(gc, s*obj.vertices[v1].x + tX, s*obj.vertices[v1].y + tY,
-		     s*obj.vertices[v2].x + tX, s*obj.vertices[v2].y + tY);
-	    drawLine(gc, s*obj.vertices[v1].x + tX, s*obj.vertices[v1].y + tY,
-		     s*obj.vertices[v3].x + tX, s*obj.vertices[v3].y + tY);
-	    drawLine(gc, s*obj.vertices[v2].x + tX, s*obj.vertices[v2].y + tY,
-		     s*obj.vertices[v3].x + tX, s*obj.vertices[v3].y + tY);
-	    
+    private void drawTriangle(Matrix A, Matrix B, Matrix C) {
+	if (A.getColLen() != 1 || B.getColLen() != 1 || C.getColLen() != 1) {
+	    return;
+	} else  if (A.getRowLen() != 2 || A.getRowLen() != 2 || C.getRowLen() != 2) {
+	    System.out.println("NOTICE: Not a 2 x 1 matrix");
 	}
+	drawLine(A.entry(0,0), A.entry(1,0), B.entry(0,0), B.entry(1,0));
+	drawLine(A.entry(0,0), A.entry(1,0), C.entry(0,0), C.entry(1,0));
+	drawLine(C.entry(0,0), C.entry(1,0), B.entry(0,0), B.entry(1,0));	
+    }
+
+    private void drawOrthographic() {
+	gc.setFill(Color.WHITE);
+	gc.fillRect(0,0, canvas.getWidth(), canvas.getHeight());
+	
+	OBJReader obj = new OBJReader("dodecahedron.obj");
+	Double transX = xTransSlider.getValue();
+	Double transY = yTransSlider.getValue();
+	Double yz = yzSlider.getValue();
+	Double xz = xzSlider.getValue();
+	Double xy = xySlider.getValue();
+	Double scale = scaleSlider.getValue();
+	//Double scale = 100.;
+	for (int i = 0; i < obj.faces.length; i++) {
+	    Vertex vert1 = obj.vertices[obj.faces[i].v1];
+	    Matrix vect1 = new Matrix(3,1);
+	    vect1.setMatrix(new Double[][] {{vert1.x}, {vert1.y}, {vert1.z}});
+	    System.out.println(vect1);
+	    vect1 = vect1.rotate(yz, xz, xy);
+	    vect1 = vect1.scale(scale);
+	    vect1 = vect1.orthoTranslate(transX, transY);
+	    Matrix orthoVect1 = Matrix.orthographic(vect1);
+	    
+	    Vertex vert2 = obj.vertices[obj.faces[i].v2];
+	    Matrix vect2 = new Matrix(3,1);
+	    vect2.setMatrix(new Double[][] {{vert2.x}, {vert2.y}, {vert2.z}});
+	    vect2 = vect2.rotate(yz, xz, xy);
+	    vect2 = vect2.scale(scale);
+	    vect2 = vect2.orthoTranslate(transX, transY);
+	    Matrix orthoVect2 = Matrix.orthographic(vect2);
+	    
+	    Vertex vert3 = obj.vertices[obj.faces[i].v3];
+	    Matrix vect3 = new Matrix(3,1);
+	    vect3.setMatrix(new Double[][] {{vert3.x}, {vert3.y}, {vert3.z}});
+	    vect3 = vect3.rotate(yz, xz, xy);
+	    vect3 = vect3.scale(scale);
+	    vect3 = vect3.orthoTranslate(transX, transY);
+	    Matrix orthoVect3 = Matrix.orthographic(vect3);
+
+	    drawTriangle(orthoVect1, orthoVect2, orthoVect3);
+	}
+
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void actionHandler() {
+	drawOrthographic();
     }
 
 }
--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -4,7 +4,7 @@ public class Matrix {
 
     private int row;
     private int col;
-    public Double[][] matrix;
+    private Double[][] matrix;
 
 
     public Matrix (int row, int col) {
@@ -18,14 +18,32 @@ public class Matrix {
 	}
     }
 
+    public int getColLen() {
+	return col;
+    }
+
+    public int getRowLen() {
+	return row;
+    }
+
+    public void setMatrix(Double[][] matrix) {
+	this.matrix = matrix;
+    }
+
+    public double entry(int r, int c) {
+	return this.matrix[r][c];
+    }
+
     public String toString() {
-	String str = "";
+	String str = "{";
 	for (int i = 0; i < this.row; i++) {
+	    str += "{";
 	    for (int j = 0; j < this.col; j++) {
 		str += this.matrix[i][j] + "\t";
 	    }
-	    str += "\n";
+	    str += "}\n";
 	}
+	str += "}";
 	return str;
     }
 
@@ -50,8 +68,8 @@ public class Matrix {
     public static Matrix yzRotation(Double t) {
 	Matrix rotate = new Matrix(3,3);
 	rotate.matrix = new Double[][]
-	    {{1., 0., 0.},
-	     {0., Math.cos(t), 0-Math.sin(t)},
+	    {{1., 0.,          0.},
+	     {0., Math.cos(t), -1*Math.sin(t)},
 	     {0., Math.sin(t), Math.cos(t)}};
 	return rotate;
     }
@@ -59,35 +77,63 @@ public class Matrix {
     public static Matrix xzRotation(Double t) {
 	Matrix rotate = new Matrix(3,3);
 	rotate.matrix = new Double[][]
-	    {{Math.cos(t), 0., Math.sin(t)},
-	     {0., 1., 0.},
-	     {0-Math.sin(t), 0., Math.cos(t)}};
+	    {{Math.cos(t),    0., Math.sin(t)},
+	     {0.,             1., 0.},
+	     {-1*Math.sin(t), 0., Math.cos(t)}};
 	return rotate;
     }
 
     public static Matrix xyRotation(Double t) {
 	Matrix rotate = new Matrix(3,3);
 	rotate.matrix = new Double[][]
-	    {{Math.cos(t), 0-Math.sin(t), 0.,},
-	     {Math.sin(t), Math.cos(t), 0.},
-	     {0., 0., 1.}};
+	    {{Math.cos(t), -1*Math.sin(t), 0.,},
+	     {Math.sin(t), Math.cos(t),    0.},
+	     {0.,          0.,             1.}};
 	return rotate;
+    }
+
+    public Matrix orthoTranslate(Double x, Double y) {
+	Matrix trans = new Matrix(row, 1);
+	trans.setMatrix(new Double[][] {{this.entry(0,0) + x}, {this.entry(1,0) + y}});
+	return trans;
+    }
+
+    public Matrix scale(Double factor) {
+	Matrix scaling = new Matrix(1, 1);
+	scaling.setMatrix(new Double[][] {{factor}});
+	try {
+	    return multiply(this, scaling);
+	} catch (Exception e) {
+	    return new Matrix(row, col);
+	}
     }
 
     public Matrix rotate(Double yz, Double xz, Double xy) {
 	try {
-	    Matrix rotatedMatrixA = multiply(this, yzRotation(yz));
-	    Matrix rotatedMatrixB = multiply(rotatedMatrixA, xzRotation(xz));
-	    Matrix rotatedMatrixC = multiply(rotatedMatrixB, xyRotation(xy));
+	    Matrix rotatedMatrixA = multiply(yzRotation(yz), this);
+	    Matrix rotatedMatrixB = multiply(xzRotation(xz), rotatedMatrixA);
+	    Matrix rotatedMatrixC = multiply(xyRotation(xy), rotatedMatrixB);
 	    return rotatedMatrixC;
 	} catch (Exception e) {
+	    System.out.println("Rotation error " + e.getMessage());
 	    return new Matrix(3, 3);
 	}
     }
 
     public static Matrix orthographic(Matrix v) {
 	Matrix ortho = new Matrix(2,1);
-	ortho.matrix = new Double[][] {{v.matrix[0][0]},{v.matrix[1][0]}};
+	ortho.setMatrix(new Double[][] {{v.entry(0,0)}, {v.entry(1,0)}});
 	return ortho;
+    }
+
+    public static Matrix perspective(double h, double w, double fov, double zNear, double zFar) {
+	Matrix pers = new Matrix(4,4);
+	double d = zFar / (zFar - zNear);
+	pers.matrix = new Double[][]
+	    {{h/w, 0., 0., 0.},
+	     {0., 1/Math.tan(fov/2), 0., 0.},
+	     {0., 0., d, -d * zNear, 0.},
+	     {0., 0., 1., 0.}};
+	return pers;
     }
 }
