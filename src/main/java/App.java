4aad5aa963977bad5bc83efbdc969fcfa2c59f46
Nicholas <nap44794@uga.edu>
2024-07-25 16:41:12 -0400

    refactor: organize node declarations and instantiations

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,108 +11,141 @@ import javafx.scene.layout.HBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.application.Platform;
 
 import java.lang.Math;
 
 public class App extends Application {
 
-    Scene scene;
-    
-    Slider yzSlider;
-    Slider xzSlider;
-    Slider xySlider;
-    Slider scaleSlider;
-    Slider xTransSlider;
-    Slider yTransSlider;
-    Slider zTransSlider;
-    Slider xPanSlider;
-    Slider yPanSlider;
-    Button update;
-    Button load;
-    TextField fileInput;
+    /**
+     *  controlBox
+     *   rotateBox
+     *    yzSliderBox (label, yzSlider)
+     *    xzSliderBox (label, xzSlider)
+     *    xySliderBox (label, xySlider)
+     *   transBox
+     *    xTransBox (label, xTransField)
+     *    yTransBox (label, yTransField)
+     *    zTransbox (label, zTransField)
+     *   panBox
+     *    xPanBox (label, xPanField)
+     *    yPanBox (label, yPanField)
+     *   zoomBox (label, zoomField)
+     *   fileInBox (label, fileInput)
+     *   updateBtn
+     */
+    private VBox controlBox;
+    private VBox rotateBox;
+    private HBox yzSliderBox;
+    private Slider yzSlider;
+    private HBox xzSliderBox;
+    private Slider xzSlider;
+    private HBox xySliderBox;
+    private Slider xySlider;    
+    private VBox transBox;
+    private HBox xTransBox;
+    private TextField xTransField;
+    private HBox yTransBox;
+    private TextField yTransField;
+    private HBox zTransBox;
+    private TextField zTransField;
+    private VBox panBox;
+    private HBox xPanBox;
+    private TextField xPanField;
+    private HBox yPanBox;
+    private TextField yPanField;
+    private HBox zoomBox;
+    private TextField zoomField;
+    private HBox fileInBox;
+    private TextField fileInput;
+    private Button updateBtn;
 
-    Canvas canvas;
-    GraphicsContext gc;
+    private Canvas canvas;
+    private GraphicsContext gc;
+
+    private BorderPane root;
+    private Scene scene;
+    private Stage stage;
+
+    public App() {
+	this.stage = null;
+	this.scene = null;
+	this.root = new BorderPane();
+	
+	this.canvas = new Canvas(1000, 1000);
+	this.gc = canvas.getGraphicsContext2D();
+	this.gc.setStroke(Color.BLACK);
+	this.gc.setLineWidth(1);
+	this.controlBox = new VBox();
+	this.rotateBox = new VBox();
+	this.yzSliderBox = new HBox();
+	this.yzSlider = new Slider(0, Math.PI * 2, 0);
+	this.xzSliderBox = new HBox();
+	this.xzSlider = new Slider(0, Math.PI * 2, 0);
+	this.xySliderBox = new HBox();
+	this.xySlider = new Slider(0, Math.PI * 2, 0);
+	this.transBox = new VBox();
+	this.xTransBox = new HBox();
+	this.xTransField = new TextField("0");
+	this.yTransBox = new HBox();
+	this.yTransField = new TextField("0");
+	this.zTransBox = new HBox();
+	this.zTransField = new TextField("0");
+	this.panBox = new VBox();
+	this.xPanBox = new HBox();
+	this.xPanField = new TextField("" + canvas.getWidth() / 2);
+	this.yPanBox = new HBox();
+	this.yPanField = new TextField("" + canvas.getHeight() / 2);
+	this.zoomBox = new HBox();
+	this.zoomField = new TextField("1");
+	this.fileInBox = new HBox();
+	this.fileInput = new TextField("yoshi.obj");
+	this.updateBtn = new Button("Update");
+	this.updateBtn.setOnAction(e -> updateHandler());
+    }
 
     @Override
-    public void start(Stage primaryStage) {
-        BorderPane root = new BorderPane();
-	
-        scene = new Scene(root, 1000, 1000);	
+    public void init() {
+	System.out.println("init() called");
+	connectNodes();
+    }
 
-        // Create a canvas
-        this.canvas = new Canvas(scene.getWidth()*2/3, scene.getHeight()*2/3);
-        gc = canvas.getGraphicsContext2D();
+    @Override
+    public void start(Stage stage) {
+	this.scene = new Scene(this.root);
 
-	HBox inputBox = new HBox();
-	fileInput = new TextField("yoshi.obj");
-	load = new Button("Load");
-	load.setOnAction(e -> loadHandler());
-	inputBox.getChildren().addAll(fileInput, load);
-	
-	
-	VBox sliderBox = new VBox(8);
-	HBox xTransBox = new HBox();
-	HBox yTransBox = new HBox();
-	HBox zTransBox = new HBox();
-	HBox xPanBox = new HBox();
-	HBox yPanBox = new HBox();
-	HBox yzBox = new HBox();
-	HBox xzBox = new HBox();
-	HBox xyBox = new HBox();
-	HBox scaleBox = new HBox();
-
-	xTransSlider = new Slider(0, 50, 0);
-	yTransSlider = new Slider(0, 50, 0);
-	zTransSlider = new Slider(0, 50, 0);
-
-	xPanSlider = new Slider(0, 500, 0);
-	yPanSlider = new Slider(0, 500, 0);
-	
-	yzSlider = new Slider(0, Math.PI * 2, 0);
-	xzSlider = new Slider(0, Math.PI * 2, 0);
-	xySlider = new Slider(0, Math.PI * 2, 0);
-	
-	scaleSlider = new Slider(0, 200, 1);
-	
-	update = new Button("update");
-	update.setOnAction(e -> actionHandler());
-
-	
-	xTransBox.getChildren().addAll(new Label("X Translation"), xTransSlider);
-	yTransBox.getChildren().addAll(new Label("Y Translation"), yTransSlider);
-	zTransBox.getChildren().addAll(new Label("Z Translation"), zTransSlider);
-	xPanBox.getChildren().addAll(new Label("X Pan"), xPanSlider);
-	yPanBox.getChildren().addAll(new Label("Y Pan"), yPanSlider);
-	
-	yzBox.getChildren().addAll(new Label("YZ Rotation"), yzSlider);
-	xzBox.getChildren().addAll(new Label("XZ Rotation"), xzSlider);
-	xyBox.getChildren().addAll(new Label("XY Rotation"), xySlider);
-	
-	scaleBox.getChildren().addAll(new Label("Scale"), scaleSlider, update);
-	
-	sliderBox.getChildren().addAll(xTransBox, yTransBox, zTransBox, xPanBox, yPanBox, yzBox, xzBox, xyBox, scaleBox);
-	
-        // Set initial color and width for drawing
-        gc.setStroke(Color.BLACK);
-        gc.setLineWidth(1);
-
-	// draws an orthographic projection
-       	//drawOrthographic();
-
-	//draws perspective
 	drawPerspective("yoshi.obj");
 
-        // Create a border pane layout
-        root.setCenter(canvas);
-	root.setBottom(sliderBox);
-	root.setTop(inputBox);
+	this.stage = stage;
+	this.stage.setOnCloseRequest(e -> Platform.exit());
+	this.stage.setTitle("OBJ Viewer");
+	this.stage.setScene(this.scene);
+	this.stage.sizeToScene();
+	this.stage.show();
+    }
 
+    @Override
+    public void stop() {
+	System.out.println("stop() called");
+    }
 
-        // Set the stage
-        primaryStage.setTitle("OBJ Viewer");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    private void connectNodes() {
+	this.root.setCenter(this.canvas);
+	this.root.setBottom(this.controlBox);
+	this.controlBox.getChildren().addAll(this.rotateBox, this.transBox, this.panBox, this.zoomBox, this.fileInBox, this.updateBtn);
+	this.rotateBox.getChildren().addAll(this.xzSliderBox, this.yzSliderBox, this.xySliderBox);
+	this.xzSliderBox.getChildren().addAll(new Label("XZ Rotate: "), this.xzSlider);
+	this.yzSliderBox.getChildren().addAll(new Label("YZ Rotate: "), this.yzSlider);
+	this.xySliderBox.getChildren().addAll(new Label("XY Rotate: "), this.xySlider);
+	this.transBox.getChildren().addAll(this.xTransBox, this.yTransBox, this.zTransBox);
+	this.xTransBox.getChildren().addAll(new Label("X Translation: "), this.xTransField);
+	this.yTransBox.getChildren().addAll(new Label("Y Translation: "), this.yTransField);
+	this.zTransBox.getChildren().addAll(new Label("Z Translation: "), this.zTransField);
+	this.panBox.getChildren().addAll(this.xPanBox, this.yPanBox);
+	this.xPanBox.getChildren().addAll(new Label("X Pan: "), this.xPanField);
+	this.yPanBox.getChildren().addAll(new Label("Y Pan: "), this.yPanField);
+	this.zoomBox.getChildren().addAll(new Label("Zoom: "), this.zoomField);
+	this.fileInBox.getChildren().addAll(new Label("File: "), this.fileInput);
     }
 
     private void drawLine(double startX, double startY, double endX, double endY) {
@@ -131,27 +164,43 @@ public class App extends Application {
     }
 
     private void drawOrthographic() {
-	gc.setFill(Color.WHITE);
-	gc.fillRect(0,0, canvas.getWidth(), canvas.getHeight());
-	
+
 	OBJReader obj = new OBJReader("yoshi.obj");
-	Double transX = xTransSlider.getValue();
-	Double transY = yTransSlider.getValue();
-	Double transZ = zTransSlider.getValue();
-	Double panX = xPanSlider.getValue();
-	Double panY = yPanSlider.getValue();
+	
+	Double transX;
+	Double transY;
+	Double transZ;
+	Double panX;
+	Double panY;
+	Double zoom;
+	
+	try {
+	    transX = Double.parseDouble(xTransField.getText());
+	    transY = Double.parseDouble(yTransField.getText());
+	    transZ = Double.parseDouble(zTransField.getText());
+	    panX = Double.parseDouble(xPanField.getText());
+	    panY = Double.parseDouble(yPanField.getText());
+	    zoom = Double.parseDouble(zoomField.getText());
+	} catch (NumberFormatException nfe) {
+	    System.out.println("non-parsable input\n" + nfe.getMessage());
+	    return;
+	} catch (NullPointerException npe) {
+	    System.out.println("empty input\n" + npe.getMessage());
+	    return;
+	}
 	Double yz = yzSlider.getValue();
 	Double xz = xzSlider.getValue();
 	Double xy = xySlider.getValue();
-	Double scale = scaleSlider.getValue();
 
+	gc.setFill(Color.WHITE);
+	gc.fillRect(0,0, canvas.getWidth(), canvas.getHeight());
+	
 	for (int i = 0; i < obj.faces.length; i++) {
 	    Vertex vert1 = obj.vertices[obj.faces[i].v1];
 	    Matrix vect1 = new Matrix(3,1);
 	    vect1.setMatrix(new Double[][] {{vert1.x}, {vert1.y}, {vert1.z}});
-	    //System.out.println(vect1);
 	    vect1 = vect1.rotate(yz, xz, xy);
-	    vect1 = vect1.scale(scale);
+	    vect1 = vect1.scale(zoom);
 	    vect1 = vect1.orthoTranslate(transX, transY);
 	    Matrix orthoVect1 = Matrix.orthographic(vect1);
 	    
@@ -159,7 +208,7 @@ public class App extends Application {
 	    Matrix vect2 = new Matrix(3,1);
 	    vect2.setMatrix(new Double[][] {{vert2.x}, {vert2.y}, {vert2.z}});
 	    vect2 = vect2.rotate(yz, xz, xy);
-	    vect2 = vect2.scale(scale);
+	    vect2 = vect2.scale(zoom);
 	    vect2 = vect2.orthoTranslate(transX, transY);
 	    Matrix orthoVect2 = Matrix.orthographic(vect2);
 	    
@@ -167,7 +216,7 @@ public class App extends Application {
 	    Matrix vect3 = new Matrix(3,1);
 	    vect3.setMatrix(new Double[][] {{vert3.x}, {vert3.y}, {vert3.z}});
 	    vect3 = vect3.rotate(yz, xz, xy);
-	    vect3 = vect3.scale(scale);
+	    vect3 = vect3.scale(zoom);
 	    vect3 = vect3.orthoTranslate(transX, transY);
 	    Matrix orthoVect3 = Matrix.orthographic(vect3);
 
@@ -177,62 +226,68 @@ public class App extends Application {
     }
 
     public void drawPerspective(String file) {
+
+	OBJReader obj = new OBJReader(this.fileInput.getText());
+	
+	Double transX;
+	Double transY;
+	Double transZ;
+	Double panX;
+	Double panY;
+	Double zoom;
+	
+	try {
+	    transX = Double.parseDouble(this.xTransField.getText());
+	    transY = Double.parseDouble(this.yTransField.getText());
+	    transZ = Double.parseDouble(this.zTransField.getText());
+	    panX = Double.parseDouble(this.xPanField.getText());
+	    panY = Double.parseDouble(this.yPanField.getText());
+	    zoom = Double.parseDouble(this.zoomField.getText());
+	} catch (NumberFormatException nfe) {
+	    System.out.println("non-parsable input\n" + nfe.getMessage());
+	    return;
+	} catch (NullPointerException npe) {
+	    System.out.println("empty input\n" + npe.getMessage());
+	    return;
+	}
+	Double yz = this.yzSlider.getValue();
+	Double xz = this.xzSlider.getValue();
+	Double xy = this.xySlider.getValue();
+
 	gc.setFill(Color.WHITE);
-	gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-
-	OBJReader obj = new OBJReader(file);
-
-	Double transX = xTransSlider.getValue();
-	Double transY = yTransSlider.getValue();
-	Double transZ = zTransSlider.getValue();
-
-	Double xPan = xPanSlider.getValue();
-	Double yPan = yPanSlider.getValue();
-
-	Double yz = yzSlider.getValue();
-	Double xz = xzSlider.getValue();
-	Double xy = xySlider.getValue();
-
-	Double scale = scaleSlider.getValue();
+	gc.fillRect(0,0, canvas.getWidth(), canvas.getHeight());
 
 	for (int i = 0; i < obj.faces.length; i++) {
 	    Vertex vert1 = obj.vertices[obj.faces[i].v1];
 	    Matrix vect1 = new Matrix(3,1);
 	    vect1.setMatrix(new Double[][] {{vert1.x}, {vert1.y}, {vert1.z}});
-	    //System.out.println(vect1);
 	    vect1 = vect1.rotate(yz, xz, xy);
 	    vect1 = vect1.persTranslate(transX, transY, transZ);
 	    vect1 = vect1.persDivide(Matrix.perspective(16, 9, 1, -5, 5));
 	    Matrix orthoVect1 = Matrix.orthographic(vect1);
-	    orthoVect1 = orthoVect1.scale(scale);
-	    orthoVect1 = orthoVect1.orthoTranslate(xPan, yPan);
+	    orthoVect1 = orthoVect1.scale(zoom);
+	    orthoVect1 = orthoVect1.orthoTranslate(panX, panY);
 	    
 	    Vertex vert2 = obj.vertices[obj.faces[i].v2];
 	    Matrix vect2 = new Matrix(3,1);
 	    vect2.setMatrix(new Double[][] {{vert2.x}, {vert2.y}, {vert2.z}});
-	    //System.out.println(vect2);
 	    vect2 = vect2.rotate(yz, xz, xy);
 	    vect2 = vect2.persTranslate(transX, transY, transZ);
 	    vect2 = vect2.persDivide(Matrix.perspective(16, 9, 1, -5, 5));
 	    Matrix orthoVect2 = Matrix.orthographic(vect2);
-	    orthoVect2 = orthoVect2.scale(scale);
-	    orthoVect2 = orthoVect2.orthoTranslate(xPan, yPan);
+	    orthoVect2 = orthoVect2.scale(zoom);
+	    orthoVect2 = orthoVect2.orthoTranslate(panX, panY);
 	    
 	    Vertex vert3 = obj.vertices[obj.faces[i].v3];
 	    Matrix vect3 = new Matrix(3,1);
 	    vect3.setMatrix(new Double[][] {{vert3.x}, {vert3.y}, {vert3.z}});
-	    //System.out.println(vect3);
 	    vect3 = vect3.rotate(yz, xz, xy);
 	    vect3 = vect3.persTranslate(transX, transY, transZ);
 	    vect3 = vect3.persDivide(Matrix.perspective(16, 9, 1, -5, 5));
 	    Matrix orthoVect3 = Matrix.orthographic(vect3);
-	    orthoVect3 = orthoVect3.scale(scale);
-	    orthoVect3 = orthoVect3.orthoTranslate(xPan, yPan);
+	    orthoVect3 = orthoVect3.scale(zoom);
+	    orthoVect3 = orthoVect3.orthoTranslate(panX, panY);
 
-
-	    System.out.println(orthoVect1 + "\n"
-			       + orthoVect2 + "\n"
-			       + orthoVect3 + "\n");
 	    drawTriangle(orthoVect1, orthoVect2, orthoVect3);
 	}
     }
@@ -241,14 +296,7 @@ public class App extends Application {
         launch(args);
     }
 
-    public void actionHandler() {
-	//drawOrthographic();
+    public void updateHandler() {
 	drawPerspective(fileInput.getText());
     }
-
-    public void loadHandler() {
-	drawPerspective(fileInput.getText());
-	System.out.println("loaded " + fileInput.getText());
-    }
-
 }
